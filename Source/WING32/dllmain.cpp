3bd21433b98bfd979721acd745df4652448a3de7
MildewMan1 <48700987+MildewMan1@users.noreply.github.com>
2021-04-20 12:24:54 -0500

    Update dllmain.cpp

--- Source/WING32/dllmain.cpp
+++ Source/WING32/dllmain.cpp
@@ -1,16 +1,41 @@
-#include "pch.h"
+#include "framework.h"
+#include <assert.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+//Microsoft Detours header files - must download Detours lib from Microsoft.
+#include <detours.h>
+#include <detver.h>
+#include <syelog.h>
+
+#include "CrashFixCode.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        assert(false);
+        DisableThreadLibraryCalls(hModule);
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        //use Microsoft Detours to detour CreateWindowExA to our custom version.
+        assert(DetourAttach(&(PVOID&)realCreateWindowExA, myCreateWindowExA) == NO_ERROR);
+
+        DetourTransactionCommit();
+
         break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
+        break;
     case DLL_PROCESS_DETACH:
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        //Detach detour when DLL is unloaded.
+        DetourDetach(&(PVOID&)realCreateWindowExA, myCreateWindowExA);
+
+        DetourTransactionCommit();
         break;
     }
     return TRUE;
 }
-
